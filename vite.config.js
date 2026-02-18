ceb439bc4858a5e71a320bce32a13d4ccffe9bde
Plugo.ia <donovanbitencourt2@gmail.com>
2026-02-18 10:35:56 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 3000,
-    allowedHosts: 'all'
-  }
+    allowedHosts: [
+      'sb-6xzivodaz5n0.vercel.run',
+    ],
+  },
 })
